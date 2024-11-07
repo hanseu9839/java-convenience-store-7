6d28e5268e61e00d95b632b43dcddf081d7f20e7
hanseounggyun <hanseu9839@gmail.com>
2024-11-07 15:42:35 +0900

    test: 실패 Product 테스트 코드 작성

--- src/test/java/store/ProductTest.java
+++ src/test/java/store/ProductTest.java
@@ -1,2 +1,15 @@
-package store;public class ProductTest {
+package store;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+
+    @Test
+    void 상품_할인_저장_테스트() {
+        String input = "콜라 1,000원 10개 탄산2+1";
+       Product product = new Product();
+
+        assertThat(product).isEqualTo(new Product("콜라", 1000, 10, "탄산2+1"));
+    }
+
 }
