61ff98737f4d59a0bd0e88f830a8dda4569cdd62
hanseounggyun <hanseu9839@gmail.com>
2024-11-07 16:24:34 +0900

    feature: ProductTest 성공 테스트 코드 작성

--- src/main/java/store/Product.java
+++ src/main/java/store/Product.java
@@ -1,2 +1,66 @@
-package store;public class Product {
+package store;
+
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+    private String description;
+
+    public Product(String name, int price, int quantity, String description) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.description = description;
+    }
+
+    public static Product from(String input) {
+        input = hasZeroQuantity(input);
+        System.out.println(input);
+        String[] productInfos = input.split(" ");
+
+        return new Product(productInfos[0], covertNumber(productInfos[1]), covertNumber(productInfos[2]), productInfos[3]);
+    }
+
+    private static String hasZeroQuantity(String input) {
+        if (input.contains("재고 없음")) {
+            return input.replaceAll("재고 없음", "0개");
+        }
+        return input;
+    }
+
+    private static int covertNumber(String productInfos) {
+        String number = productInfos.replaceAll("[^0-9]", "");
+        System.out.println(number);
+        return Integer.parseInt(number);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return quantity == product.quantity && price == product.price && Objects.equals(name, product.name) && Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, quantity, price, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
 }
--- src/test/java/store/ProductTest.java
+++ src/test/java/store/ProductTest.java
@@ -1,15 +1,25 @@
 package store;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class ProductTest {
 
     @Test
     void 상품_할인_저장_테스트() {
         String input = "콜라 1,000원 10개 탄산2+1";
-       Product product = new Product();
+       Product product = Product.from(input);
 
         assertThat(product).isEqualTo(new Product("콜라", 1000, 10, "탄산2+1"));
+    }
+
+    @Test
+    void 싱품_재고_없음_저장_테스트() {
+        String input = "오렌지주스 1,800원 재고 없음 주스2+1";
+        Product product = Product.from(input);
+        assertThat(product).isEqualTo(new Product("오렌지주스", 1800, 0, "주스2+1"));
     }
 
 }
